9e739c28421f0b997197e6f94c58c4778c4c8eb0
Amberlee <amberleec12@yahoo.com>
2017-06-09 13:39:21 -0500

    initial commit

--- src/main/java/LoginSerlvet.java
+++ src/main/java/LoginSerlvet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginSerlvet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -26,3 +27,4 @@ public class LoginSerlvet extends HttpServlet {
         }
     }
 }
+
